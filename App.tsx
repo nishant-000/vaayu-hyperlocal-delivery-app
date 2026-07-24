ff8b8691cc4de41ad7ae99d2bf711161e6104c44
nizokio <coder6701@gmail.com>
2026-07-24 22:07:43 +0530

    Fix syntax error in App.tsx styles (justifyContent)

--- App.tsx
+++ App.tsx
@@ -459,7 +459,7 @@ const styles = StyleSheet.create({
   },
   tabPressable: {
     alignItems: "center",
-    justify.content: "center",
+    justifyContent: "center",
   },
   pill: {
     flexDirection: "row",
